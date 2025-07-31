d0b594bb934f07ebd68159a6e2c1a7db7f5ebfbb
hacxy <hacxy.js@outlook.com>
2025-07-31 20:20:21 +0800

    feat: Update eslint config and add cli-ink template

---
+++ templates/cli-ink/src/app.tsx
@@ -0,0 +1,13 @@
+import { Box, Text } from 'ink';
+import React from 'react';
+import Screen from './components/Screen/index.js';
+
+export default function App() {
+  return (
+    <Screen>
+      <Box>
+        <Text>Hello world</Text>
+      </Box>
+    </Screen>
+  );
+}
