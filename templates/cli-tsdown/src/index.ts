60c5b19a654ff8ff9749e79731d8f805ba620a2b
hacxy <hacxy.js@outlook.com>
2025-08-01 18:10:24 +0800

    fix: Add tsx and execa as dev dependencies

--- templates/cli-tsdown/src/index.ts
+++ templates/cli-tsdown/src/index.ts
@@ -3,6 +3,10 @@ import pkg from '../package.json';
 
 function bootstrap() {
   const cli = cac(pkg.name);
+  cli.command('hello').action(() => {
+    console.log('Hello world');
+  });
+
   cli.version(pkg.version);
   cli.help();
   cli.parse(process.argv);
