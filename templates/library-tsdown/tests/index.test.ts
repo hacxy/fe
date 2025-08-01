5f6f2c72082566decb84c5ce5cb10738a944e948
hacxy <hacxy.js@outlook.com>
2025-08-01 17:36:18 +0800

    chore: Add test case for foo function

--- templates/library-tsdown/tests/index.test.ts
+++ templates/library-tsdown/tests/index.test.ts
@@ -1,6 +1,10 @@
 import { expect, it } from 'vitest';
-import { sum } from '../src/index.js';
+import { foo, sum } from '../src/index.js';
 
 it('sum', () => {
   expect(sum(1, 2)).toEqual(3);
+});
+
+it('foo', () => {
+  expect(foo('Hacxy')).toEqual('Hello Hacxy');
 });
